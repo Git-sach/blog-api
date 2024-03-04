787e8560e7f18d58256caf378bc98f4af21e8c77
Adrien Cheynet <gitcha@outlook.fr>
2024-03-04 16:48:17 +0100

    Ajout de la configuration pour éviter les CORS

--- src/main/java/com/adriencheynet/blog/BlogApplication.java
+++ src/main/java/com/adriencheynet/blog/BlogApplication.java
@@ -7,8 +7,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BlogApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BlogApplication.class, args);
-		System.out.println("J'ai démaré");
+		try {
+			String MYSQL_HOST = System.getenv("MYSQL_HOST");
+			System.out.println("MYSQL_HOST : " + MYSQL_HOST);
+	
+			String MYSQL_USERNAME = System.getenv("MYSQL_USERNAME");
+			System.out.println("MYSQL_USERNAME : " + MYSQL_USERNAME);
+	
+			String MYSQL_PASSWORD = System.getenv("MYSQL_PASSWORD");
+			System.out.println("MYSQL_PASSWORD : " + MYSQL_PASSWORD);
+	
+			SpringApplication.run(BlogApplication.class, args);
+		} catch (Exception e) {
+			
+			System.out.println("Erreur de connexion");
+		}
 	}
 
 }
